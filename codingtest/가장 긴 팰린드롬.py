43147aa36c4a65c3f83f1988b9afb68b4fd12a17
rapsby <gsu5022@gmail.com>
2020-12-14 13:27:56 +0900

    add numpy practice

--- codingtest/가장 긴 팰린드롬.py
+++ codingtest/가장 긴 팰린드롬.py
@@ -1,12 +1,20 @@
+def isPalindrome(s):
+    return s == s[::-1]
 def solution(s):
     if len(s) <= 1:
         return len(s)
     i,l=0,0
     for j in range(len(s)):
-        if s[j-l: j+1] == s[j-l: j+1][::-1]:
+        print(j,i,l, s[i:l])
+        if isPalindrome(s[j-l: j+1]):
+            print(1)
             i, l = j-l, l+1
-        elif j-l > 0 and s[j-l-1: j+1] == s[j-l-1: j+1][::-1]:
+            print(s[i:i+l])
+        elif j-l > 0 and isPalindrome(s[j-l-1: j+1]):
+            print(2)
             i, l = j-l-1, l+2
+            print(s[i:i+l])
+        print('>',j,i,l, s[i:i+l])
+        
     return len(s[i: i+l])
-
-print(solution('a'))
+print(solution('abcdcba'))
